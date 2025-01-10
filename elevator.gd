f1d4c6d77c67933199b4d8a3a12cb0e7261f72a8
Adam Socki <socki.adam@gmail.com>
2025-01-10 16:55:17 +0100

    elevator ending positioner

--- elevator.gd
+++ elevator.gd
@@ -1,14 +1,29 @@
-extends Area2D
+@tool
 
+extends Area2D
+var end_position_node: Node2D
+
+@export var vertical_position_01: float:
+	set(value):
+		vertical_position_01 = value
+		# Update position when the value changes in editor
+		if Engine.is_editor_hint() and end_position_node:
+			end_position_node.position.y = value
 
 func _ready():
-	pass # Replace with function body.
+	# Get the node reference
+	end_position_node = $PositionPoint01
+	
+	if Engine.is_editor_hint():
+		# Update position in editor
+		if end_position_node:
+			end_position_node.position.y = vertical_position_01
 
-
-func _process(delta):
-	pass
 
 
 func _on_body_entered(body):
-	if body.get_name() == "Player":
-		print("player")
+	if Engine.is_editor_hint():
+		pass
+	else:
+		if body.get_name() == "Player":
+			print("player")
